b26190abef0d7d8ee22b8fe7fca98f27f65854b7
Jason Scott <jpscott07@gmail.com>
2024-09-12 12:30:44 -0400

    get example building but linking issues remain

--- symlink.sh
+++ symlink.sh
@@ -21,6 +21,12 @@ find . -type l -delete
 ln -s $(pwd)/build-systems $(pwd)/examples/ti/lp-f28379d/bme280/build-systems;
 
 # Provide project root to examples as a subproject.
+#
+# While developing this repo (embedded-c-hal), the changes won't be included
+# through the wrap dependency because only the HEAD of main will be pulled from
+# GitHub. To solve this, the root of the repo is symlinked into the subprojects
+# dir, so the current state of the local files is included instead of the remote
+# files from the repo on GitHub. 
 # 
 ln -s $(pwd) $(pwd)/examples/ti/lp-f28379d/bme280/subprojects/embedded-hal-c;
 
